78cff281e48ea1b7347666fee5794d8702231095
Hyeonho Jang <138095361+Hyeonhooo@users.noreply.github.com>
2023-07-10 15:15:05 +0900

    Add email domain

--- GUI.py
+++ GUI.py
@@ -24,12 +24,16 @@ def add_student():
     id = int(entry_id.get())
     department = entry_department.get()
     phone_number = entry_phone.get()
-    email = entry_email.get()
+    email_id = entry_email_id.get()
+    email_domain = email_var.get()
+    email = f"{email_id}@{email_domain}"
     arr.append(Student(id, department, phone_number, email))
     entry_id.delete(0, tk.END)
     entry_department.delete(0, tk.END)
     entry_phone.delete(0, tk.END)
-    entry_email.delete(0, tk.END)
+    entry_email_id.delete(0, tk.END)
+    email_var.set("직접입력")
+    entry_email_id.configure(state=tk.NORMAL)
     save_data()
 
 def delete_student():
@@ -58,6 +62,21 @@ def search_student():
         result_text.set("Student ID not found")
     entry_id.delete(0, tk.END)
 
+def update_student():
+    id = int(entry_id.get())
+    new_id = int(entry_new_id.get())
+    found = False
+    for j in range(len(arr)):
+        if arr[j].id == id:
+            arr[j].id = new_id
+            result_text.set("Student ID updated")
+            found = True
+            break
+    if not found:
+        result_text.set("Student ID not found")
+    entry_id.delete(0, tk.END)
+    entry_new_id.delete(0, tk.END)
+
 def display_students():
     result_text.config(state=tk.NORMAL)
     result_text.delete(1.0, tk.END)
@@ -65,6 +84,15 @@ def display_students():
     for student in arr:
         result_text.insert(tk.END, f"ID: {student.id}, Department: {student.department}, Phone: {student.phone_number}, Email: {student.email}\n")
     result_text.config(state=tk.DISABLED)
+
+def select_email_domain(event):
+    selected_domain = email_var.get()
+    if selected_domain == "직접입력":
+        entry_email_id.configure(state=tk.NORMAL)
+    else:
+        entry_email_id.delete(0, tk.END)
+        entry_email_id.insert(tk.END, "")
+        entry_email_id.configure(state=tk.DISABLED)
 
 root = tk.Tk()
 root.title("Student ID Manager")
@@ -90,32 +118,58 @@ entry_phone.grid(row=2, column=1, padx=5, pady=5)
 label_email = tk.Label(root, text="Email:")
 label_email.grid(row=3, column=0, padx=5, pady=5)
 
-entry_email = tk.Entry(root)
-entry_email.grid(row=3, column=1, padx=5, pady=5)
+entry_email_id = tk.Entry(root)
+entry_email_id.grid(row=3, column=1, padx=5, pady=5)
+
+email_var = tk.StringVar(root)
+email_var.set("직접입력")
+email_domain_options = [
+    "직접입력",
+    "naver.com",
+    "hanmail.net",
+    "hotmail.com",
+    "nate.com",
+    "hufs.ac.kr",
+    "yahoo.co.kr",
+    "daum.com",
+    "paran.com",
+    "hanafos.com",
+    "empal.com",
+    "gmail.com",
+    "korea.com",
+    "hanmir.com"
+]
+email_dropdown = tk.OptionMenu(root, email_var, *email_domain_options, command=select_email_domain)
+email_dropdown.grid(row=3, column=2, padx=5, pady=5)
+
+label_at = tk.Label(root, text="@")
+label_at.grid(row=3, column=1, padx=5, pady=5)
 
 button_add = tk.Button(root, text="Add Student ID", command=add_student)
-button_add.grid(row=4, column=0, columnspan=2, padx=5, pady=5)
+button_add.grid(row=4, column=0, columnspan=4, padx=5, pady=5)
 
 button_delete = tk.Button(root, text="Delete Student ID", command=delete_student)
-button_delete.grid(row=5, column=0, columnspan=2, padx=5, pady=5)
+button_delete.grid(row=5, column=0, columnspan=4, padx=5, pady=5)
 
 button_search = tk.Button(root, text="Search Student ID", command=search_student)
-button_search.grid(row=6, column=0, columnspan=2, padx=5, pady=5)
+button_search.grid(row=6, column=0, columnspan=4, padx=5, pady=5)
+
+button_update = tk.Button(root, text="Update Student ID", command=update_student)
+button_update.grid(row=7, column=0, columnspan=4, padx=5, pady=5)
 
 button_display = tk.Button(root, text="Display Student IDs", command=display_students)
-button_display.grid(row=7, column=0, columnspan=2, padx=5, pady=5)
+button_display.grid(row=8, column=0, columnspan=4, padx=5, pady=5)
 
-result_text = tk.Text(root, height=10, width=100)
-result_text.grid(row=0, column=2, rowspan=9, padx=10, pady=5, sticky="nsew")
-result_text.config(state=tk.DISABLED)
+result_text = tk.Text(root, height=25, width=50)
+result_text.grid(row=0, column=4, rowspan=9, padx=10, pady=5)
 
 scrollbar = tk.Scrollbar(root)
-scrollbar.grid(row=0, column=3, rowspan=9, padx=0, pady=5, sticky="ns")
+scrollbar.grid(row=0, column=5, rowspan=9, padx=0, pady=5, sticky="ns")
 result_text.config(yscrollcommand=scrollbar.set)
 scrollbar.config(command=result_text.yview)
 
 button_exit = tk.Button(root, text="Exit", command=root.destroy)
-button_exit.grid(row=9, column=0, columnspan=2, padx=5, pady=5)
+button_exit.grid(row=9, column=0, columnspan=4, padx=5, pady=5)
 
 arr = []
 
